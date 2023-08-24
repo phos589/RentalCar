87e1e33514c7ab9db8d264d6ef2143c8a35c56b0
Stefan C <140889919+phos589@users.noreply.github.com>
2023-08-24 11:14:29 +0200

    update the website

---
+++ Rental Car/script/data.js
@@ -0,0 +1,10 @@
+export const models = [{
+  img: '../image/Model/Mercedes.png',
+  model: 'Mercedes-AMG GLE53',
+  Mark:'Mercedes',
+  Year:'2021',
+  AC: 'Yes',
+  Trabsmission:'Automatuc',
+  fule:'electric',
+
+}]

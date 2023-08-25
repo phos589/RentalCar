f5e5b9d8f5d95253290ad092bc681ec51d5cbf84
Stefan C <140889919+phos589@users.noreply.github.com>
2023-08-25 09:23:09 +0200

    website done

--- Rental Car/script/Rental-Car.js
+++ Rental Car/script/Rental-Car.js
@@ -1,2 +1,12 @@
-import {models} from '../script/data.js'
+const questions = document.querySelectorAll(".question");
 
+questions.forEach(function(question) {
+  question.addEventListener("click", function() {
+    const answer = this.nextElementSibling;
+    if (answer.style.display === "none") {
+      answer.style.display = "block";
+    } else {
+      answer.style.display = "none";
+    }
+  });
+});

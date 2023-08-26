aa7dc3c68f786584d0885e6b17bf942a2a8337d0
Stefan C <140889919+phos589@users.noreply.github.com>
2023-08-26 09:57:30 +0200

    car model
    rent a car fleet selector

--- Rental Car/script/Rental-Car.js
+++ Rental Car/script/Rental-Car.js
@@ -1,12 +1,31 @@
-const questions = document.querySelectorAll(".question");
+import { models } from "../script/data.js";
 
-questions.forEach(function(question) {
-  question.addEventListener("click", function() {
-    const answer = this.nextElementSibling;
-    if (answer.style.display === "none") {
-      answer.style.display = "block";
-    } else {
-      answer.style.display = "none";
+let rentalModel = document.querySelector('.rentalModel-js');
+const carImage = rentalModel.querySelector('.ModelCar');
+const modelModel = rentalModel.querySelector('#modelModel');
+const modelNameElement = rentalModel.querySelector('#modelName');
+const modelYearElement = rentalModel.querySelector('#modelYear');
+const modelACElement = rentalModel.querySelector('#modelAC');
+const modelTransmissionElement = rentalModel.querySelector('#modelTransmission');
+const modelFuelElement = rentalModel.querySelector('#modelFuel');
+
+const buttons = document.querySelectorAll('.button');
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const clickedModel = button.textContent;
+
+    // Find the matching model object from the models array
+    const selectedModel = models.find(model => model.Mark === clickedModel);
+
+    if (selectedModel) {
+      carImage.src = selectedModel.img;
+      modelModel.textContent = `Model: ${selectedModel.model}`;
+      modelNameElement.textContent = `Mark: ${selectedModel.Mark}`;
+      modelYearElement.textContent = `Year: ${selectedModel.Year}`;
+      modelACElement.textContent = `AC: ${selectedModel.AC}`;
+      modelTransmissionElement.textContent = `Transmission: ${selectedModel.Trabsmission}`;
+      modelFuelElement.textContent = `Fuel: ${selectedModel.fule}`;
     }
   });
 });
--- Rental Car/script/data.js
+++ Rental Car/script/data.js
@@ -1,10 +1,21 @@
 export const models = [{
   img: '../image/Model/Mercedes.png',
-  model: 'Mercedes-AMG GLE53',
+  model: 'Mercedes-Benz S-Class',
   Mark:'Mercedes',
   Year:'2021',
   AC: 'Yes',
   Trabsmission:'Automatuc',
   fule:'electric',
 
-}]
+},
+{
+  img: '../image/Model/Porsche.png',
+  model: 'Porsche 911',
+  Mark:'Porsche',
+  Year:'2019',
+  AC: 'Yes',
+  Trabsmission:'Automatuc',
+  fule:'disel',
+
+}
+]
